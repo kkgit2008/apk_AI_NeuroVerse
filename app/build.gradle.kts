23af544a09f0652b38b9e9fc7af8410f781625fe
supervip <172490109+kkgit2008@users.noreply.github.com>
2025-09-02 10:31:17 +0800

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.ksp)
 
+
     kotlin("plugin.serialization") version "2.1.21"
 }
 val localPropertiesFile = rootProject.file("local.properties")
