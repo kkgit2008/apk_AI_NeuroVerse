a0a896ff3709700186a16ea76843688918831b65
supervip <172490109+kkgit2008@users.noreply.github.com>
2025-09-11 20:43:58 +0800

    更新 build.gradle.kts

--- ai-core/build.gradle.kts
+++ ai-core/build.gradle.kts
@@ -42,12 +42,12 @@ android {
             }
         }
     }
-    externalNativeBuild {
-        cmake {
-            path("src/main/cpp/CMakeLists.txt")
-            version = "3.22.1"
-        }
-    }
+    //externalNativeBuild {
+        //cmake {
+            //path("src/main/cpp/CMakeLists.txt")
+            //version = "3.22.1"
+        //}
+    //}
     buildTypes {
         release {
             isMinifyEnabled = false
